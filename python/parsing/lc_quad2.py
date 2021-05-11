af08575b3bc0ae45236cb84030860193882b632c
Samar-Gh <samar.shahabi.g@gmail.com>
2021-05-11 10:27:56 +0200

    Add files via upload

---
+++ python/parsing/lc_quad2.py
@@ -0,0 +1,54 @@
+import json
+import re
+
+from common.container.qapair import QApair
+from common.container.uri import Uri
+from kb.dbpedia import DBpedia
+
+from parsing.answerparser import AnswerParser
+
+
+class LC_Qaud2:
+    def __init__(self, path="./data/LC-QUAD2/data.json"):
+        self.raw_data = []
+        self.qapairs = []
+        self.path = path
+        self.parser = LC_QaudParser2()
+
+    def load(self):
+        with open(self.path) as data_file:
+            self.raw_data = json.load(data_file)
+
+    def parse(self):
+        parser = LC_QaudParser2()
+        for raw_row in self.raw_data:
+            sparql_query = raw_row["sparql_query"].replace("DISTINCT COUNT(", "COUNT(DISTINCT ")
+            self.qapairs.append(
+                QApair(raw_row["corrected_question"], [], sparql_query, raw_row, raw_row["_id"], self.parser))
+
+    def print_pairs(self, n=-1):
+        for item in self.qapairs[0:n]:
+            print(item)
+            print("")
+
+
+class LC_QaudParser2(AnswerParser):
+    def __init__(self):
+        super(LC_QaudParser2, self).__init__(DBpedia(one_hop_bloom_file="./data/blooms/spo1.bloom"))
+
+    def parse_question(self, raw_question):
+        return raw_question
+
+    def parse_sparql(self, raw_query):
+        uris = [Uri(raw_uri, DBpedia.parse_uri) for raw_uri in re.findall('<[^>]*>', raw_query)]
+
+        return raw_query, True, uris
+
+    def parse_answerset(self, raw_answerset):
+        return []
+
+    def parse_answerrow(self, raw_answerrow):
+        return []
+
+    def parse_answer(self, answer_type, raw_answer):
+        return "", None
